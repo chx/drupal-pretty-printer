dab3b6f0f817e2963009383f945ee4a1a6d00871
Karoly Negyesi <chx1975@gmail.com>
2025-05-13 20:54:13 +0200

    yay single printer

--- .github/workflows/generate.php
+++ .github/workflows/generate.php
@@ -1,6 +1,6 @@
 <?php
 
-use DrupalPrettyPrinter\DrupalPrettyPrinterBase;
+use DrupalPrettyPrinter\DrupalPrettyPrinter;
 use PhpParser\ParserFactory;
 
 require_once './vendor/autoload.php';
@@ -8,6 +8,6 @@ require_once './vendor/autoload.php';
 $file = file_get_contents(__DIR__ . '/user.module');
 $parser = (new ParserFactory())->createForHostVersion();
 $stmts = $parser->parse($file);
-$printer = DrupalPrettyPrinterBase::getPrinter(['html' => FALSE]);
+$printer = new DrupalPrettyPrinter(['html' => FALSE]);
 $output = $printer->prettyPrintFile($stmts);
 file_put_contents('user1.module', $output);
