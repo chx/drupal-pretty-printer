5a2ea6bf00b43599308e055eb700f794dea64d41
Karoly Negyesi <chx1975@gmail.com>
2025-05-14 11:33:42 +0200

    it seems format preserving is a v5 feature

--- .github/workflows/generate.php
+++ .github/workflows/generate.php
@@ -12,24 +12,26 @@ $code = file_get_contents(__DIR__ . '/user.module');
 // prepares for format preserving printing.
 [$printer, $stmts] = DrupalPrettyPrinter::getPrinterAndParse($code);
 
-// Do something with these statements. We are testing the printer here so no
-// need to change anything.
+// Normal code would change $stmts but for testing the printer no changes
+// are necessary.
 $newCode = $printer->prettyPrintFile($stmts);
 file_put_contents('user1.module', $newCode);
+// For v4, there's nothing else do.
+if (!class_exists(\PhpParser\Parser\Php8::class)) {
+  exit(0);
+}
 
-// To test the format preserving printer, statements need to change otherwise
-// it will just copy the original code verbatim.
+// To test the format preserving printer in v5, statements need to change
+// otherwise it will just copy the original code verbatim.
 changeStmtsForTesting($stmts);
-
 $newCode = $printer->printFormatPreserving($stmts);
-
-// This is again for testing only.
 $count = substr_count($newCode, DRUPAL_PRETTY_PRINTER_TEST);
 printf("Changed %d nodes.\n", $count);
+file_put_contents('user2.module', str_replace(DRUPAL_PRETTY_PRINTER_TEST, '', $newCode));
 exit($count && $code === str_replace(DRUPAL_PRETTY_PRINTER_TEST, '', $newCode) ? 0 :1);
 
 function changeStmtsForTesting($stmts): void {
-  $traverser = (new \PhpParser\NodeTraverser);
+  $traverser = new \PhpParser\NodeTraverser;
   $traverser->addVisitor(new class extends \PhpParser\NodeVisitorAbstract {
     public function enterNode(\PhpParser\Node $node) {
       if (isset($node->name)) {
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 This is a drop in replacement for `PhpParser\PrettyPrinter\Standard` printing
 Drupal coding style.
-See [generate.php](.github/workflows/generate.php) for an example on how
-to use this. 
+
+Recommended usage:
+
+````
+[$printer, $stmts] = DrupalPrettyPrinter::getPrinterAndParse($code);
+````
+
+This parses the code and returns the printer and the parsed statements. It recognizes PHP 8.2 syntax for parsing.
+The printer is ready for a `$printer->printFormatPreserving($stmts)` call, however this seems to only work well with
+nikic/php-parser v5. In my testing, for v4 the output was the same as with `$printer->prettyPrintFile($stmts)`.
